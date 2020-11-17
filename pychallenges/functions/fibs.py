097e821cb1c07453aefb853363dd8a809dfcdd5b
Pedro <schreiber.mmb@gmail.com>
2020-11-17 13:17:04 -0800

    finish python fibs w/ tests

--- pychallenges/functions/fibs.py
+++ pychallenges/functions/fibs.py
@@ -1,15 +1,22 @@
 def at(n):
-    n1, n2, count = 0, 1, 0
-    if n >= 1:
-        return n
-    while count < n:
-        n1 = n2
-        n2 = n1 + n2
-        count = count + 1
-    return n1 + n2
+    if n <= 0:
+        return 0
+    else:
+        n1,n2 = 0,1
+        for i in range(1,n):
+            n1,n2 = n2, n1+n2
+        return n2
 
 def take(n):
-    return 1
+    return [at(x + 1) for x in range(n)]
 
-def sum_evens(max):
-    return 1
+def sum_evens(limit):
+    counter = 1
+    current = at(counter)
+    res = 0
+    while (current < limit):
+        if current % 2 == 0:
+            res = res + current
+        counter += 1
+        current = at(counter)
+    return res
--- pychallenges/tests/test_fibs.py
+++ pychallenges/tests/test_fibs.py
@@ -3,12 +3,19 @@ from functions import fibs
 
 class TestFibs(unittest.TestCase):
 
-    def get_number_n_in_fibonacci_sequence(self):
+    def test_get_number_n_in_fibonacci_sequence(self):
         self.assertEqual(fibs.at(0), 0)
         self.assertEqual(fibs.at(1), 1)
         self.assertEqual(fibs.at(2), 1)
         self.assertEqual(fibs.at(12), 144)
-        self.assertEqual(fibs.at(12), 377)
+        self.assertEqual(fibs.at(15), 610)
+    
+    def test_get_fibonacci_sequence_up_to_nth_element(self):
+        self.assertEqual(fibs.take(10), [1,1,2,3,5,8,13,21,34,55])
+
+    def test_get_sum_of_even_fibs_below_x(self):
+        self.assertEqual(fibs.sum_evens(55), 2+8+34)
+        self.assertEqual(fibs.sum_evens(4000000), 4613732)
 
 if __name__ == '__main__':
     unittest.main()
